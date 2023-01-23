15618a9535c3a5a2252ea50db72ca96a333cd538
Icyeong <dbcjswjs@gmail.com>
2023-01-24 06:23:58 +0900

    라우터 설정 및 페이지 이동 연결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Navbar from './components/common/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Work from './pages/Work';
+import { MainWrapper, PageWrapper } from './styles';
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <PageWrapper>
+         <BrowserRouter>
+            <Navbar />
+            <MainWrapper>
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/work" element={<Work />} />
+                  <Route path="/contact" element={<Contact />} />
+               </Routes>
+            </MainWrapper>
+         </BrowserRouter>
+      </PageWrapper>
+   );
 }
 
 export default App;
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,69 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Navbar = () => {
+   return (
+      <NavWrapper>
+         <ul>
+            <li>
+               <NavLink to="/" active="active">
+                  Icyeong
+               </NavLink>
+            </li>
+            <li>
+               <NavLink to="/about" active="active">
+                  About
+               </NavLink>
+            </li>
+            <li>
+               <NavLink to="/work" active="active">
+                  Work
+               </NavLink>
+            </li>
+            <li>
+               <NavLink to="/contact" active="active">
+                  Contact
+               </NavLink>
+            </li>
+         </ul>
+      </NavWrapper>
+   );
+};
+
+const NavWrapper = styled.div`
+   position: fixed;
+   top: 0;
+   left: 0;
+   z-index: 10;
+   width: 130px;
+   height: 100vh;
+   display: flex;
+   align-items: center;
+   ul {
+      width: 100%;
+      height: fit-content;
+      background-color: #161616;
+      display: flex;
+      flex-direction: column;
+      padding: 15px 0;
+   }
+   a {
+      display: block;
+      border-top: 1px solid #1d1d1d;
+      padding: 12px;
+      text-align: center;
+      cursor: pointer;
+      &last-child {
+         border-bottom: 1px solid #1d1d1d;
+      }
+      color: white;
+      font-size: 15px;
+      font-weight: 600;
+      text-align: center;
+   }
+   a.active {
+      border-left: 2px solid #b8e829;
+   }
+`;
+
+export default Navbar;
