21a8e80459ff881997c247c76914c838e20d3505
Icyeong <dbcjswjs@gmail.com>
2023-01-28 03:05:00 +0900

    stack 이미지 넣기

--- src/dummyData/stack.js
+++ src/dummyData/stack.js
@@ -1,14 +1,31 @@
+import react from '../assets/image/stack/react.png';
+import next from '../assets/image/stack/next.png';
+import javascript from '../assets/image/stack/javascript.png';
+import es6 from '../assets/image/stack/es6.png';
+import redux from '../assets/image/stack/redux.png';
+import recoil from '../assets/image/stack/recoil.png';
+import html from '../assets/image/stack/html.png';
+import css from '../assets/image/stack/css.png';
+import styled from '../assets/image/stack/styledComponents.png';
+import bootstrap from '../assets/image/stack/bootstrap.png';
+import jquery from '../assets/image/stack/jquery.png';
+import github from '../assets/image/stack/github.png';
+import git from '../assets/image/stack/git.png';
+import figma from '../assets/image/stack/figma.png';
+
 export const stack_dummyData = [
-   { stack: 'React', src: '/' },
-   { stack: 'Next.js', src: '/' },
-   { stack: 'JavaScript(ES6+)', src: '/' },
-   { stack: 'Redux', src: '/' },
-   { stack: 'Recoil', src: '/' },
-   { stack: 'HTML5', src: '/' },
-   { stack: 'CSS3', src: '/' },
-   { stack: 'Styled-components', src: '/' },
-   { stack: 'Bootstrap5', src: '/' },
-   { stack: 'JQuery', src: '/' },
-   { stack: 'Github', src: '/' },
-   { stack: 'Figma', src: '/' }
+   { stack: 'React', src: { react }, srcName: 'react' },
+   { stack: 'Next.js', src: { next }, srcName: 'next' },
+   { stack: 'Redux', src: { redux }, srcName: 'redux' },
+   { stack: 'Recoil', src: { recoil }, srcName: 'recoil' },
+   { stack: 'Styled-components', src: { styled }, srcName: 'styled' },
+   { stack: 'HTML5', src: { html }, srcName: 'html' },
+   { stack: 'CSS3', src: { css }, srcName: 'css' },
+   { stack: 'JavaScript', src: { javascript }, srcName: 'javascript' },
+   { stack: 'ES6+', src: { es6 }, srcName: 'es6' },
+   { stack: 'JQuery', src: { jquery }, srcName: 'jquery' },
+   { stack: 'Bootstrap5', src: { bootstrap }, srcName: 'bootstrap' },
+   { stack: 'Git', src: { git }, srcName: 'git' },
+   { stack: 'Github', src: { github }, srcName: 'github' },
+   { stack: 'Figma', src: { figma }, srcName: 'figma' }
 ];
