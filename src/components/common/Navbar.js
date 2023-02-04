cbe81658c54e5ab01c346d3fd16f9b954eaf3135
Icyeong <dbcjswjs@gmail.com>
2023-02-05 01:59:58 +0900

    navbar 스타일 추가

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,27 +1,30 @@
+import { useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Navbar = () => {
+   const ulRef = useRef();
+   const [isStretched, setStretchState] = useState(false);
    return (
       <NavWrapper>
-         <ul>
+         <ul ref={ulRef} className={isStretched ? 'stretch' : null}>
             <li>
-               <NavLink to="/" active="active">
+               <NavLink to="/" active="active" onClick={() => setStretchState(false)}>
                   Icyeong
                </NavLink>
             </li>
             <li>
-               <NavLink to="/about" active="active">
+               <NavLink to="/about" active="active" onClick={() => setStretchState(true)}>
                   About
                </NavLink>
             </li>
             <li>
-               <NavLink to="/work" active="active">
+               <NavLink to="/work" active="active" onClick={() => setStretchState(true)}>
                   Work
                </NavLink>
             </li>
             <li>
-               <NavLink to="/contact" active="active">
+               <NavLink to="/contact" active="active" onClick={() => setStretchState(true)}>
                   Contact
                </NavLink>
             </li>
@@ -34,18 +37,23 @@ const NavWrapper = styled.div`
    position: fixed;
    top: 0;
    left: 0;
-   z-index: 10;
+   z-index: 15;
    width: 130px;
    height: 100vh;
    display: flex;
    align-items: center;
    ul {
       width: 100%;
-      height: fit-content;
+      height: 210px;
       background-color: #161616;
       display: flex;
       flex-direction: column;
+      justify-content: center;
       padding: 15px 0;
+      transition: 0.3s;
+      &.stretch {
+         height: 100%;
+      }
    }
    a {
       display: block;
