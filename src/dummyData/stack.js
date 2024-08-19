0ac42d3b0bbb1ffbc9e04bb4a0692678f6dafce7
chaeyeong Imm <cyim@chonf.co.kr>
2024-08-19 17:01:46 +0900

    stack: 기술 추가

--- src/dummyData/stack.js
+++ src/dummyData/stack.js
@@ -1,26 +1,31 @@
 import react from '../assets/image/stack/react.png';
 import next from '../assets/image/stack/next.png';
 import javascript from '../assets/image/stack/javascript.png';
+import typescript from '../assets/image/stack/typescript.png';
 import es6 from '../assets/image/stack/es6.png';
 import redux from '../assets/image/stack/redux.png';
 import recoil from '../assets/image/stack/recoil.png';
+import zustand from '../assets/image/stack/zustand.png';
+import tanstack from '../assets/image/stack/tanstack.png';
 import html from '../assets/image/stack/html.png';
 import css from '../assets/image/stack/css.png';
 import styled from '../assets/image/stack/styledComponents.png';
 import bootstrap from '../assets/image/stack/bootstrap.png';
 import jquery from '../assets/image/stack/jquery.png';
 import github from '../assets/image/stack/github.png';
+import gitlab from '../assets/image/stack/gitlab.png';
 import git from '../assets/image/stack/git.png';
 import figma from '../assets/image/stack/figma.png';
 
 export const stack_dummyData = [
    { stack: 'React', src: { react }, srcName: 'react' },
    { stack: 'Next.js', src: { next }, srcName: 'next' },
-   { stack: 'Zustand', src: { recoil }, srcName: 'zustand' },
+   { stack: 'Zustand', src: { zustand }, srcName: 'zustand' },
    { stack: 'Recoil', src: { recoil }, srcName: 'recoil' },
    { stack: 'Redux', src: { redux }, srcName: 'redux' },
+   { stack: 'TanStack-Query', src: { tanstack }, srcName: 'tanstack' },
    { stack: 'Styled-components', src: { styled }, srcName: 'styled' },
-   { stack: 'TypeScript', src: { javascript }, srcName: 'typescript' },
+   { stack: 'TypeScript', src: { typescript }, srcName: 'typescript' },
    { stack: 'HTML5', src: { html }, srcName: 'html' },
    { stack: 'CSS3', src: { css }, srcName: 'css' },
    { stack: 'JavaScript', src: { javascript }, srcName: 'javascript' },
@@ -29,6 +34,6 @@ export const stack_dummyData = [
    { stack: 'Bootstrap5', src: { bootstrap }, srcName: 'bootstrap' },
    { stack: 'Git', src: { git }, srcName: 'git' },
    { stack: 'Github', src: { github }, srcName: 'github' },
-   { stack: 'Gitlab', src: { github }, srcName: 'gitlab' },
+   { stack: 'Gitlab', src: { gitlab }, srcName: 'gitlab' },
    { stack: 'Figma', src: { figma }, srcName: 'figma' }
 ];
