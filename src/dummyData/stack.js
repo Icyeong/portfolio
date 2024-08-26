40b824b455106e5e76c1507e53152bffaee7258b
chaeyeong Imm <cyim@chonf.co.kr>
2024-08-26 16:41:34 +0900

    update: 포트폴리오 업데이트

--- src/dummyData/stack.js
+++ src/dummyData/stack.js
@@ -1,5 +1,6 @@
 import react from '../assets/image/stack/react.png';
 import next from '../assets/image/stack/next.png';
+import vue from '../assets/image/stack/vue.png';
 import javascript from '../assets/image/stack/javascript.png';
 import typescript from '../assets/image/stack/typescript.png';
 import es6 from '../assets/image/stack/es6.png';
@@ -20,10 +21,11 @@ import figma from '../assets/image/stack/figma.png';
 export const stack_dummyData = [
    { stack: 'React', src: { react }, srcName: 'react' },
    { stack: 'Next.js', src: { next }, srcName: 'next' },
+   { stack: 'Vue.js', src: { vue }, srcName: 'vue' },
+   { stack: 'React-Query', src: { tanstack }, srcName: 'tanstack' },
    { stack: 'Zustand', src: { zustand }, srcName: 'zustand' },
    { stack: 'Recoil', src: { recoil }, srcName: 'recoil' },
    { stack: 'Redux', src: { redux }, srcName: 'redux' },
-   { stack: 'TanStack-Query', src: { tanstack }, srcName: 'tanstack' },
    { stack: 'Styled-components', src: { styled }, srcName: 'styled' },
    { stack: 'TypeScript', src: { typescript }, srcName: 'typescript' },
    { stack: 'HTML5', src: { html }, srcName: 'html' },
